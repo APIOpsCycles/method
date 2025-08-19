00c03986943ef22e93aafe1430f8269dd034f01e
Marjukka Niinioja <mniinio@users.noreply.github.com>
2025-08-19 08:44:14 +0300

    remove popup form, adding global banner that shows if no page specific banner

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,10 @@ export const collections = {
                                 stations: z.array(z.string()).optional(),
                                 category: z.string().optional(),
                                 canvasId: z.string().optional(),
+                                // Add a default value to the built-in `banner` field.
+                                banner: z.object({ content: z.string() }).default({
+                                  content: 'APIOps meetups are back! Join us for the next one, more info at <a href="https://www.apiops.info/">apiops.info</a>.',
+                                }),
                         }),
                 }),
         }),
