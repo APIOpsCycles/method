5e5603ed18c91d081d4080fa6331bad6050c7d64
Marjukka Niinioja <mniinio@users.noreply.github.com>
2025-11-10 09:58:18 +0200

    update content banner

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,7 +14,7 @@ export const collections = {
                                 canvasId: z.string().optional(),
                                 // Add a default value to the built-in `banner` field.
                                 banner: z.object({ content: z.string() }).default({
-                                  content: 'APIOps meetups are back! Join us for the next one, more info at <a href="https://www.apiops.info/">apiops.info</a>.',
+                                  content: 'Join the APIOps community, more info at <a href="https://www.apiops.info/">apiops.info</a>.',
                                 }),
                         }),
                 }),
